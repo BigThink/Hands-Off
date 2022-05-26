a6c8ad55bd4f7567361b56d46a702bdb4add4482
Jnguyenpham94 <nguyenjonathan94@yahoo.com>
2022-05-25 18:58:56 -0700

    Help menu update from test version
    Help menu is more or less complete

--- output_gui/Help.py
+++ output_gui/Help.py
@@ -3,9 +3,9 @@ import PySimpleGUI as sg
 
 
 def help_window():
-    sg.theme('darkBlack1')
+    sg.theme('darkBlue1')
     top = [
-        [sg.Text("Help window last updated 05/21/2022")],
+        [sg.Text("Help window last updated 05/25/2022")],
     ]
     developer = [[sg.Text("This program was developed by Brian Vuong, Sam Blawski, "
                           "Dylan Watson, Jonathan Nguyen-Pham, Dening Zhang")],
@@ -25,28 +25,64 @@ def help_window():
                         "The profiles contains profile mappings from the developers and allows for user created and"
                         " duplicate profile")],
                     [sg.Text("The register gesture button allows for gestures to be captured and named for later use")],
-                    [sg.Text("The gesture's data are uploaded to the firebase by user desire")]]
+                    [sg.Text("The gesture's data are uploaded to the firebase by user desire")]
+                    ]
 
-    instructions = [[sg.Text("To register a gesture:")],
-                    [sg.Text("1. Press the Start button to turn on the camera ")],
-                    [sg.Text("2. Press Register Gesture. A window will popup, name the gesture, press confirm")],
-                    [sg.Text(
-                        "3. Hold up a gesture before the 5 second countdown finishes, that gesture will now be included"
-                        " in the profile window as a gesture to include in a profile")]]
+    settings = [[sg.Text(
+        "The Settings sub-menu controls which camera is being used for input and output, debug mode and Hand Match Confidence")],
+        [sg.Text("Use the dropdown menu to change the being camera used")],
+        [sg.Text("Check debug mode to show the wireframe and uncheck to NOT show wireframe")],
+        [sg.Text(
+            "Hand Match Confidence will determine how rigorous the camera and the program will match the hand gesture (input a number between 0 to 1) a higher value will be more picky while a lower value will be loose with its certainty")]]
+
+    reg_gesture = [[sg.Text("To register a gesture:")],
+                   [sg.Text("1. Press the Start button to turn on the camera ")],
+                   [sg.Text("2. Press Register Gesture. A window will popup, name the gesture, press confirm")],
+                   [sg.Text(
+                       "3. Hold up a gesture before the 5 second countdown finishes, that gesture will now be included in the profile window as a gesture to be added to a profile")]]
+
+    profile = [[sg.Text("Profiles:")],
+               [sg.Text(
+                   "The profiles sub-menu contains various buttons that allow the creation and editting of profiles")],
+               [sg.Text("As well as the capability to duplicate and delete profiles already created")],
+               [sg.Text("Currently the Import from File button is not implemented")],
+               [sg.Text(
+                   "It would have allowed a file to be pulled and included in the program to allow an externally created profile to be used in your current program.")]
+               ]
+
+    action = [[sg.Text("From this sub-menu you can create action mappings to various files and shortcuts")],
+              [sg.Text("Use the Open App button to create a path to a file to later map it in profiles")],
+              [sg.Text("By either copy and pasting a file path or using the file explorer to map to that file")],
+              [sg.Text("Use the Macros button to create a button shortcut")],
+              [sg.Text(
+                  "Press Record Macro then press a set of key(s) (like 'a' or 'ctrl+c') then press Stop Recording to capture the set of keys, then press submit to save it for a later profile mapping")],
+              [sg.Text("Remember to hit Save after creating your actions!!")]]
+
+    instructions = [[sg.TabGroup([[sg.Tab("Settings", settings),
+                                   sg.Tab("Profiles", profile),
+                                   sg.Tab("Register Gesture", reg_gesture),
+                                   sg.Tab("Actions", action)
+                                   ]],
+                                 selected_background_color="DarkGreen",
+                                 tab_background_color="Gray"
+                                 )
+                     ]]
 
     layout3 = [[sg.Text("Planned modes (unfortunately removed due to time constraints): ")],
-               [sg.Text(
-                   "Cursor mode: camera will track and control mouse cursor.      Two-Hands Mode: camera will track two hands       "
-                   "Combo Mode: Sequential combination of gestures leading to a mapped action")]]
+               [sg.Text("Cursor mode: camera will track and control mouse cursor.")],
+               [sg.Text("Two-Hands Mode: camera will track two hands")],
+               [sg.Text("Combo Mode: Sequential combination of gestures leading to a mapped action")]
+               ]
 
-    Tabs = [[sg.TabGroup(
-        [[sg.Tab('Developer Details', developer, border_width=10,
-                 tooltip='Developer details'),
-          sg.Tab("Program Details", prog_details),
-          sg.Tab('Instructions', instructions),
-          sg.Tab('Modes', layout3, tooltip='Info on modes (NOT IMPLEMENTED)')]], tab_location='centertop',
-        tab_background_color='Gray', selected_title_color='Red',
-        selected_background_color='DarkBlue', border_width=5)]]
+    Tabs = [[sg.Text("Set/create a profile with the mappings in a profile then just press Start to turn on camera to allow the gestures to be used for stuff")],
+            [sg.TabGroup([[sg.Tab('Developer Details', developer, border_width=10,
+                                  tooltip='Contact details for developers'),
+                           sg.Tab("Program Details", prog_details, tooltip="Program specifics"),
+                           sg.Tab('Instructions', instructions),
+                           sg.Tab('Modes', layout3, tooltip='Info on Modes (NOT IMPLEMENTED)')]],
+                         tab_location='center',
+                         tab_background_color='Gray', selected_title_color='Red',
+                         selected_background_color='DarkBlue', border_width=5)]]
 
     layout = [
         [sg.Column(top)],
